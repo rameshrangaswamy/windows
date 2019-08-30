58d0224a49ef4e2ac3784d013801ea49c6ecb3b6
rameshrangaswamy <rameshrangaswamy1@gmail.com>
2019-08-30 14:07:34 +0530

    test

---
+++ jenkins.groovy
@@ -0,0 +1,24 @@
+def server = Artifactory.server "ArtifactDemo"
+
+def buildInfo = Artifactory.newBuildInfo()
+
+node{
+stage('checkout & package'){
+		checkout scm
+		
+		sh ("${MAVEN_HOME}//bin//mvn clean packge")
+		
+stage('publish Artifact'){
+
+		def uploadSpec = """{
+			"files": [
+				{
+				  "pattern": "jenkins-pipeline-examples/resources/.*A[A-z]tifactory.*.zip",
+				  "target": "libs-snapshot-local/",
+				  "regexp": "true"
+				}
+			]
+		}"""
+		server.upload spec: uploadSpec, buildInfo: buildInfo
+}
+}
