4c57ec40a203611fe99ebe892c0b6f8cf6af9f57
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-08-30 14:20:22 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,12 +1,12 @@
-def server = Artifactory.server "Artifact"
-def MAVEN_HOME
+def server = Artifactory.server "ArtifactDemo"
+
 def buildInfo = Artifactory.newBuildInfo()
 
 node{
 stage('checkout & package'){
 		checkout scm
 		
-		sh ("mvn clean package")
+		sh ("${MAVEN_HOME}//bin//mvn clean packge")
 		}
 		
 stage('publish Artifact'){
@@ -14,7 +14,7 @@ stage('publish Artifact'){
 		def uploadSpec = """{
 			"files": [
 				{
-				  "pattern": "jenkins-pipeline-examples/resources/.*A[A-z]tifactory.*.zip",
+				  "pattern": "/target/*.jar",
 				  "target": "libs-snapshot-local/",
 				  "regexp": "true"
 				}
