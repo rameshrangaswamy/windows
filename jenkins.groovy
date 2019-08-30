6f35f11d3aa56928a46c4aad2c323b53d0c943c6
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-08-30 17:12:28 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -15,7 +15,7 @@ stage('publish Artifact'){
 		def uploadSpec = """{
 			"files": [
 				{
-				  "pattern": "C:/Program Files (x86)/Jenkins/workspace/JFROG_PIPE/target/windows-1.0-SNAPSHOT.jar",
+				  "pattern": "**/*.jar",
 				  "target": "libs-snapshot-local"
 				}
 			]
