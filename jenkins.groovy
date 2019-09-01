0124d94c40fdd976be5b0519daa9c470dbc22d54
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-09-01 15:44:48 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,60 +1,34 @@
-#!groovy
-import groovy.sql.Sql
-import groovy.json.JsonParserType
-import groovy.json.JsonSlurperClassic
-import groovy.json.JsonOutput
-import java.io.*
-import java.net.*
-import org.apache.commons.io.FileUtils
-import java.util.*;
-import groovy.json.JsonSlurper
-
-echo "Build.Groovy file successfully loaded!!"
-
-@NonCPS
-def buildInfo = Artifactory.newBuildInfo()
-def server = Artifactory.server 'Artifact'
-
 node{
-
 stage('checkout & package'){
-		checkout scm
-		
+		checkout scm		
 		sh ("mvn clean package")
-		}
-		
+		}		
 stage('publish Artifact'){
-
-try
-{	
-		script{
-		
-		def uploadSpec = """{
-			"files": [
+	try
+	{	
+		    rtUpload (
+			serverId: "Artifact",
+			spec:
+			    """{
+			      "files": [
 				{
-				  "pattern": "**/target/*.jar",
+				  "pattern": "C:/Jenkins/workspace/ArtifactDownload/libs-snapshot-local/demo-b490.tar",
 				  "target": "libs-snapshot-local/"
 				}
-			]
-		}"""
-		server.upload(uploadSpec)
+			     ]
+			    }"""
+		    )
+	}
+	catch(Exception exception) 
+	{
+		currentBuild.result = "FAILURE"
+		throw exception
+	}
 
-		// Publish build information.
-		buildInfo.env.capture = true
-		
-		server.publishBuildInfo(buildInfo)
-		}
-}
-	   		catch(Exception exception) 
-			{
-				currentBuild.result = "FAILURE"
-				throw exception
-			}
-			
-			finally
-			{
-				println("Exiting Publish to Artifactory stage")
-			}
+	finally
+	{
+		println("Exiting Publish to Artifactory stage")
+	}
 
 }
 }
